6519cef0d9287c251a82f29763231f5c2c6b0eb3
Daniel Holth <dholth@gmail.com>
2016-05-04 20:05:26 +0000

    add rudimentary test for loader

---
+++ couchdb/tests/loader.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2016 Daniel Holth
+# All rights reserved.
+#
+# This software is licensed as described in the file COPYING, which
+# you should have received as part of this distribution.
+
+import unittest
+import os.path
+
+from couchdb import loader
+from couchdb.tests import testutil
+
+# loaded by couchdbkit with _attachments and couchapp keys removed:
+expected = {
+ '_id': u'_design/loader',
+ 'filters': {'filter': u'function(doc, req) { return true; }'},
+ 'language': u'javascript',
+ 'views': {'a': {'map': u'function(doc) {\n  emit(doc.property_to_index);\n}'}}}
+
+class LoaderTestCase(unittest.TestCase):
+
+    def test_loader(self):
+        directory = os.path.join(os.path.dirname(__file__), '_loader')
+        doc = loader.load_design_docs(directory, strip_files=True)
+        self.assertTrue(doc == expected, (doc, expected))
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(LoaderTestCase))
+    return suite
+
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
